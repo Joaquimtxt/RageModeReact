aafa1c2728876526961cf42add01f48340a4d84f
Luiz G. <luiz.mwlo76@gmail.com>
2025-04-02 17:04:54 -0300

    Conserto no Footer e Header - Construção dos containers de posts

--- src/Pages/Games/Games.jsx
+++ src/Pages/Games/Games.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+
 
 const Games = () => {
   return (
---
+++ src/components/ForumContainer/ForumContainer.jsx
@@ -0,0 +1,138 @@
+import styles from './ForumContainer.module.css'
+
+const ForumContainer = () => {
+  return (
+    <div className='container-fluid w-75'>
+
+        <div className=' border border-light rounded-3 my-4'>
+            <div className='row'>
+                <div className='col-md-7'>
+                    <div className='container'>
+
+                    <div className={`${styles.infoPost} d-flex gap-3 mx-2 mt-2 p-1`}>
+                        <img src='https://placehold.co/50x50' className='img rounded-5'></img>
+                        <h5 className='text-center py-2'>@DaigoUmahara</h5>
+                    <p className='fw-lighter mt-2'>Data, Mês, Ano</p>
+                    <p className='bg-danger ms-auto px-1 pt-1 rounded-1 mt-1 fw-medium text-start'> TAG1 TAG2 TAG3 TAG4 TAG5 TAG6</p>
+                    </div>
+                        <div className={`${styles.postContent} ms-4 text-start`}>
+
+                    <div className='  fs-3 fw-bolder mt-2 mb-3'>
+                       TITULO DA POSTAGEM DO FORUM
+                    </div>
+                    <div className=''>
+
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi unde consequatur quas exercitationem,
+                                 optio cumque atque qui facilis necessitatibus facere velit doloremque itaque, explicabo, minus quos eius 
+                                 ullam hic sapiente.
+                    
+                    </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-5 text-end '>
+                    <img src='https://placehold.co/610x343' className='img-fluid rounded-1' />
+                </div>
+            </div>
+        </div>
+
+        <div className=' border border-light rounded-3 my-4'>
+            <div className='row'>
+                <div className='col-md-7'>
+                    <div className='container'>
+
+                    <div className={`${styles.infoPost} d-flex gap-3 mx-2 mt-2 p-1`}>
+                        <img src='https://placehold.co/50x50' className='img rounded-5'></img>
+                        <h5 className='text-center py-2'>@DaigoUmahara</h5>
+                    <p className='fw-lighter mt-2'>Data, Mês, Ano</p>
+                    <p className='bg-danger ms-auto px-1 pt-1 rounded-1 mt-1 fw-medium text-start'> TAG1 TAG2 TAG3 TAG4 TAG5 TAG6</p>
+                    </div>
+                        <div className={`${styles.postContent} ms-4 text-start`}>
+
+                    <div className='  fs-3 fw-bolder mt-2 mb-3'>
+                       TITULO DA POSTAGEM DO FORUM
+                    </div>
+                    <div className=''>
+
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi unde consequatur quas exercitationem,
+                                 optio cumque atque qui facilis necessitatibus facere velit doloremque itaque, explicabo, minus quos eius 
+                                 ullam hic sapiente.
+                    
+                    </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-5 text-end '>
+                    <img src='https://placehold.co/610x343' className='img-fluid rounded-1' />
+                </div>
+            </div>
+        </div>
+
+        <div className=' border border-light rounded-3 my-4'>
+            <div className='row'>
+                <div className='col-md-7'>
+                    <div className='container'>
+
+                    <div className={`${styles.infoPost} d-flex gap-3 mx-2 mt-2 p-1`}>
+                        <img src='https://placehold.co/50x50' className='img rounded-5'></img>
+                        <h5 className='text-center py-2'>@DaigoUmahara</h5>
+                    <p className='fw-lighter mt-2'>Data, Mês, Ano</p>
+                    <p className='bg-danger ms-auto px-1 pt-1 rounded-1 mt-1 fw-medium text-start'> TAG1 TAG2 TAG3 TAG4 TAG5 TAG6</p>
+                    </div>
+                        <div className={`${styles.postContent} ms-4 text-start`}>
+
+                    <div className='  fs-3 fw-bolder mt-2 mb-3'>
+                       TITULO DA POSTAGEM DO FORUM
+                    </div>
+                    <div className=''>
+
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi unde consequatur quas exercitationem,
+                                 optio cumque atque qui facilis necessitatibus facere velit doloremque itaque, explicabo, minus quos eius 
+                                 ullam hic sapiente.
+                    
+                    </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-5 text-end '>
+                    <img src='https://placehold.co/610x343' className='img-fluid rounded-1' />
+                </div>
+            </div>
+        </div>
+
+        <div className=' border border-light rounded-3 my-4'>
+            <div className='row'>
+                <div className='col-md-7'>
+                    <div className='container'>
+
+                    <div className={`${styles.infoPost} d-flex gap-3 mx-2 mt-2 p-1`}>
+                        <img src='https://placehold.co/50x50' className='img rounded-5'></img>
+                        <h5 className='text-center py-2'>@DaigoUmahara</h5>
+                    <p className='fw-lighter mt-2'>Data, Mês, Ano</p>
+                    <p className='bg-danger ms-auto px-1 pt-1 rounded-1 mt-1 fw-medium text-start'> TAG1 TAG2 TAG3 TAG4 TAG5 TAG6</p>
+                    </div>
+                        <div className={`${styles.postContent} ms-4 text-start`}>
+
+                    <div className='  fs-3 fw-bolder mt-2 mb-3'>
+                       TITULO DA POSTAGEM DO FORUM
+                    </div>
+                    <div className=''>
+
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi unde consequatur quas exercitationem,
+                                 optio cumque atque qui facilis necessitatibus facere velit doloremque itaque, explicabo, minus quos eius 
+                                 ullam hic sapiente.
+                    
+                    </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-5 text-end '>
+                    <img src='https://placehold.co/610x343' className='img-fluid rounded-1' />
+                </div>
+            </div>
+        </div>  
+    </div>
+  )
+}
+
+export default ForumContainer
