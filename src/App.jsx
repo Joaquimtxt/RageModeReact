48a8e5a89d872656aea17d1872d35f427b2ed912
LuizG <luiz.mwlo76@gmail.com>
2025-05-20 21:25:40 -0300

    Deletando index.css para resolver conflitos - Ajustes no App.jsx (Adição de + Routes) - Ajustes no Footer - Criação do CreatePost para o usuário criar uma nova discussão + classes no App.css - Moldando a pagina de Register

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
+import "bootstrap-icons/font/bootstrap-icons.css"
 import { BrowserRouter as Router, Route, Routes } from "react-router";
 import Home from "./Pages/Home/Home";
 import Games from "./Pages/Games/Games";
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-
+import Register from './Pages/Register/Register';
+import CreatePost from './Pages/Posts/CreatePost';
 function App() {
 
   return (
@@ -16,6 +18,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/games" element={<Games/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/sendpost" element={<CreatePost/>} />
     </Routes>
     <Footer/>
   </Router>
